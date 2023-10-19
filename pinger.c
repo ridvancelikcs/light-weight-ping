93e1cc9cdf4603866414fe61805c32b1c5c90f5e
Rıdvan Çelik <ridvan.celik.cs@gmail.com>
2023-10-19 12:40:39 +0300

    Update pinger.c
    +close(socket)

--- pinger.c
+++ pinger.c
@@ -50,26 +50,28 @@ int send_ping(char *ip)
 		return (0);
 	}
 	
-	int ping_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (ping_sockfd < 0)
+	int ping_sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (ping_sock_fd < 0)
 	{
 		printf("Socket file descriptor not received!!\n");
 		return 0;
 	}
 
 	int ttl_val = 128;
-	if (setsockopt(ping_sockfd, SOL_IP, IP_TTL, &ttl_val, sizeof(int)) != 0)
+	if (setsockopt(ping_sock_fd, SOL_IP, IP_TTL, &ttl_val, sizeof(int)) != 0)
 	{
 		printf("Set socket option TTL failed! value: %d\n", ttl_val);
+		close(ping_sock_fd);
 		return 0;
 	}
 	
 	struct timeval tv_out;
 	tv_out.tv_sec = RECV_TIMEOUT;
 	tv_out.tv_usec = 0;
-	if (setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv_out, sizeof(struct timeval)) != 0)
+	if (setsockopt(ping_sock_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv_out, sizeof(struct timeval)) != 0)
 	{
 		printf("Set socket option TIMEOUT failed! value: %ld\n", tv_out.tv_sec);
+		close(ping_sock_fd);
 		return 0;
 	}
 
@@ -88,9 +90,10 @@ int send_ping(char *ip)
 
 	// send packet
 	clock_gettime(CLOCK_MONOTONIC, &time_start);
-	if (sendto(ping_sockfd, &x_pckt, sizeof(x_ping_pckt_t), 0, (struct sockaddr *)&ping_addr, sizeof(struct sockaddr_in)) <= 0)
+	if (sendto(ping_sock_fd, &x_pckt, sizeof(x_ping_pckt_t), 0, (struct sockaddr *)&ping_addr, sizeof(struct sockaddr_in)) <= 0)
 	{
 		printf("Packet Sending Failed!\n");
+		close(ping_sock_fd);
 		return (0);
 	}
 
@@ -98,9 +101,9 @@ int send_ping(char *ip)
 	r_ping_pckt_t	r_pckt;
 	memset(&r_pckt, 0x0, sizeof(r_ping_pckt_t));
 	
-	struct sockaddr_in	r_addr;
-	unsigned int		addr_len = sizeof(struct sockaddr_in);
-	int 				recv_ret = recvfrom(ping_sockfd, &r_pckt, sizeof(r_ping_pckt_t), 0, NULL, NULL);
+	struct sockaddr_in r_addr;
+	unsigned int addr_len = sizeof(struct sockaddr_in);
+	int recv_ret = recvfrom(ping_sock_fd, &r_pckt, sizeof(r_ping_pckt_t), 0, NULL, NULL);
 	
 	clock_gettime(CLOCK_MONOTONIC, &time_end);
 	double elapsed_in_usec = ((double)(time_end.tv_nsec - time_start.tv_nsec)) / 1000.0;
@@ -109,14 +112,17 @@ int send_ping(char *ip)
 	if (recv_ret <= 0) 
 	{
 		printf("Packet receive failed!\n");
+		close(ping_sock_fd);
 		return (0);
 	}
 	else if (r_pckt.icmp_hdr.type != ICMP_ECHOREPLY || strcmp(ECHO_MESSAGE, r_pckt.msg))
 	{
 		printf("Error: ICMP Type: %d, Code: %d, Message: %s", r_pckt.icmp_hdr.code, r_pckt.icmp_hdr.type, r_pckt.msg);
+		close(ping_sock_fd);
 		return (0);
 	}
 	printf("%d bytes form %s msg_seq=%d ttl=%d time=%f usec.\n", recv_ret, ip, msg_count, ttl_val, elapsed_in_usec);
+	close(ping_sock_fd);
 	return (int)elapsed_in_usec;
 }
 
